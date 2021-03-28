e409f6e81216e3680969f28fedaf6a5ef8c673ad
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-28 12:13:30 -0400

    create personal data object

---
+++ src/data.js
@@ -0,0 +1,81 @@
+export default data = {
+  name: "Armeen Hadizadeh",
+  occupation: "Full-Stack Developer",
+  imageSrc: "./assets/landing_photo.jpeg",
+  description:
+    "Recent graduate from the Lighthouse Labs Web Development Bootcamp, looking for work",
+  education: [
+    {
+      school: "Lighthouse Labs",
+      degree: "Web Development Certificate",
+      imageSrc:
+        "https://news.coinsquare.com/wp-content/uploads/2018/05/lighthouse-labs.png",
+    },
+    {
+      school: "Ryerson University",
+      degree: "Mechatronics Engineering",
+      imageSrc: "./assets/Ryerson-rgb.ong",
+    },
+  ],
+  projects: [
+    {
+      name: "Stonks",
+      description:
+        "Stock application that correlates trends in a specific stock to relevant news in that time period",
+      stack:
+        "React.js, React-Vis, SCSS, Material-UI, Firebase Authentication, Node.js, Express.js, Socket.io, Twilio",
+    },
+    {
+      name: "Binderz",
+      description:
+        "Designed and developed a resource wall similar to Pinterest, intended for students to share, like, and create resources with each other",
+      stack: "jQuery, SCSS, Node.js, Express.js, PostgreSQL",
+    },
+    {
+      name: "Scheduler",
+      description:
+        "Scheduler Application developed with React and WebSockets to allow for students to create meetings with mentors in real time",
+      stack: "React.js, Axios, Storybook, Cypress, Jest",
+    },
+  ],
+  experience: [
+    {
+      name: "Global DWS",
+      position: "Technical Consultant",
+      work:
+        "Managed a project team focused on developing solutions to streamline student reading assessments in order to relieve teacher burden, enable regular evaluation, and track students progress for faculty and parents.",
+      location: "Toronto, ON",
+      date: "Aug. 2019 to Oct. 2020",
+    },
+    {
+      name: "Orange Gate Consulting Inc.",
+      position: "Software Engineer, Intern",
+      work:
+        "Redesigned and implemented new system architecture for android applications toincrease available memory by 12%, allowing for more complex processes to be executedwithout bu􀁽er",
+      location: "Toronto, ON",
+      date: "Aug. 2018 to July. 2019",
+    },
+  ],
+  skills: {
+    languages: [
+      "Javascript",
+      "Java",
+      "C",
+      "HTML/CSS",
+      "SCSS",
+      "Python",
+      "Ruby",
+    ],
+    frameworks: [
+      "Node.js",
+      "React.js",
+      "Express.js",
+      "Material-UI",
+      "WebSockets",
+      "jQuery",
+      "Socket.io",
+      "Ruby on Rails",
+    ],
+    databases: ["PostgreSQL", "MongoDB"],
+  },
+};
