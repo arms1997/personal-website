cc1a0bb4178ca3f69ee93fdfdc80fa3cf5478ca1
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-30 19:59:00 -0400

    workin on a flip card for the landing page

---
+++ src/components/FlipCard/FlipCard.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import { useSpring, animated } from "react-spring";
+
+export default function Card() {
+  const [flipped, setFlipped] = useState(false);
+
+  const { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
+    config: { mass: 5, tension: 500, friction: 80 },
+  });
+}
