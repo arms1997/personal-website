538d6c3750c3884c98b97690ccf6b0438547d480
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-31 16:37:39 -0400

    implementing router

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import { Paper } from "@material-ui/core";
 
 import Landing from "./components/Landing";
+import FlipCard from "./components/FlipCard/FlipCard";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Paper>
-        <Landing />
+        <Router>
+          <Switch>
+            <Route path="/landing" component={Landing} />
+            <Route path="/" component={FlipCard} />
+          </Switch>
+        </Router>
       </Paper>
     </ThemeProvider>
   );
