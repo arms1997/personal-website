7b8c9b16461c3904354350b9ee83fe72ac83130b
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-04-01 11:08:57 -0400

    remove FlipCard route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import { Paper } from "@material-ui/core";
 
 import Landing from "./components/Landing";
-import FlipCard from "./components/FlipCard/FlipCard";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -12,8 +11,7 @@ function App() {
       <Paper>
         <Router>
           <Switch>
-            <Route path="/landing" component={Landing} />
-            <Route path="/" component={FlipCard} />
+            <Route path="/" component={Landing} />
           </Switch>
         </Router>
       </Paper>
